eeb9047f183df5a7eafa9515d658e3b3e0661654
sadea <almog@microfocus.com>
2021-06-19 18:28:11 +0300

    intuit demo social player implementation

--- src/components/SocialPlayer/SocialPlayer.jsx
+++ src/components/SocialPlayer/SocialPlayer.jsx
@@ -13,6 +13,11 @@ const SocialPlayer = (props) => {
             <button className="logout" onClick={props.onDisconnect}>Disconnect</button>
         </div>
     );
+    const Load = (props) => (
+        <div>
+            <button className="logout" onClick={props.onLoad}>Load</button>
+        </div>
+    );
     const handleAddSong = (url) => {
         const id = new Date().getTime().toString(); // unique number
         if (playlist.findIndex(item => item.url === url) > -1) {
@@ -23,6 +28,25 @@ const SocialPlayer = (props) => {
     };
     const handleSongEnded = (song) => {
         updatePlaylist(song);
+    };
+    const handleOnLoad = () => {
+        let id = new Date().getTime().toString(); // unique number
+        addSong({url: 'https://www.youtube.com/watch?v=sLPt028Iao8',id});
+        id = new Date().getTime().toString(); // unique number
+        setTimeout(_ => {
+            id = new Date().getTime().toString(); // unique number
+            addSong({url: 'https://www.youtube.com/watch?v=kiqEEr7CK2g',id});
+            console.log(id);
+        },800)
+        setTimeout(_ => {
+            id = new Date().getTime().toString(); // unique number
+            addSong({url: 'https://www.youtube.com/watch?v=AzfZ7hLR17s',id});
+            console.log(id);
+        },1200)
+
+     //   addSong({url: 'https://www.youtube.com/watch?v=kiqEEr7CK2g'}, id);
+     //    id = new Date().getTime().toString(); // unique number
+    //    addSong({url: 'https://www.youtube.com/watch?v=AzfZ7hLR17s'}, id);
     };
 
 
@@ -35,6 +59,7 @@ const SocialPlayer = (props) => {
             <div data-testid="player-container" className="player-container">
                 <Player song={getCurrent()} onSongEnded={handleSongEnded}/>
             </div>
+            <Load onLoad={handleOnLoad}/>
             <Logout/>
         </div>
     );
